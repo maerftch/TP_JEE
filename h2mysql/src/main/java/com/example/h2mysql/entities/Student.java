14b63d83b23abcb59effe76336efa4b80ce2ea26
hjou <hind.joudairi.2711@gmail.com>
2023-06-03 15:36:46 +0100

    tp

---
+++ h2mysql/src/main/java/com/example/h2mysql/entities/Student.java
@@ -0,0 +1,15 @@
+package com.example.h2mysql.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Table(name="EMSI_STUDENTS")
+@Data @AllArgsConstructor@NoArgsConstructor
+
+public class Student {
+
+
+}
---
+++ h2mysql/src/test/java/com/example/h2mysql/H2mysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.h2mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2mysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
